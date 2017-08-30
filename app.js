89a0c043c3761d01042097a723a4ec8ccdba75c2
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-29 23:23:14 -0700

    create

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require('./routes/routes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes/routes");
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/muber", { useMongoClient: true });
 
 // middleware
 app.use(bodyParser.json());
